f305656b0ea4a2e85e0ae64873bf46936e31e6fa
DESKTOP-JIH8QEO\Patrick Arashiro <patrick.arashiro@pequenasempresas.et>
2019-05-28 19:31:53 -0300

    <feat>: Settings in MVC

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
-package login;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.awt.Color;
-import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
@@ -27,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
         //you can use underdecorated or transparent.
         stage.initStyle(StageStyle.TRANSPARENT);
         //stage.initStyle(StageStyle.UNDERDECORATED);
